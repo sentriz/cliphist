f5a241fb42c66a4a0898bd2d5490ce6122dc6027
sentriz <senan@senan.xyz>
2023-05-06 21:17:54 +0100

    fuzz it
    why not

--- cliphist_test.go
+++ cliphist_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"image"
 	"image/jpeg"
@@ -47,5 +50,47 @@ func TestScripts(t *testing.T) {
 			env.Vars = append(env.Vars, fmt.Sprintf("XDG_CACHE_HOME=%s", env.WorkDir))
 			return nil
 		},
+	})
+}
+
+func FuzzStoreList(f *testing.F) {
+	home := f.TempDir()
+	f.Setenv("HOME", home)
+	f.Setenv("XDG_CACHE_HOME", home)
+
+	f.Fuzz(func(t *testing.T, in []byte) {
+		if len(bytes.TrimSpace(in)) == 0 {
+			return
+		}
+
+		fail := func(f string, a ...any) {
+			t.Fatalf("input %s: %s", base64.StdEncoding.EncodeToString(in), fmt.Sprintf(f, a...))
+		}
+
+		if err := store(bytes.NewReader(in), 0, 5); err != nil {
+			fail("store: %v", err)
+		}
+
+		var previewBuff bytes.Buffer
+		if err := list(&previewBuff); err != nil {
+			fail("list: %v", err)
+		}
+
+		previewScanner := bufio.NewScanner(&previewBuff)
+		previewScanner.Scan()
+
+		firstLine := previewScanner.Bytes()
+		if len(firstLine) == 0 {
+			fail("no line")
+		}
+
+		var out bytes.Buffer
+		if err := decode(bytes.NewReader(firstLine), &out); err != nil {
+			fail("decode: %v", string(firstLine), err)
+		}
+
+		if !bytes.Equal(in, out.Bytes()) {
+			fail("not equal")
+		}
 	})
 }
